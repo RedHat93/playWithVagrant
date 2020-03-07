c79eaa776a14a0749f793f3ae56669c48affaf9b
RedHat93 <andrea.fancellu@live.it>
2020-03-07 19:06:20 +0100

    added ssl support

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,27 @@ if ! [ -L /var/www ]; then
   ln -fs /vagrant /var/www
 fi
 
-cp -f /vagrant/000-default.conf /etc/apache2/sites-enabled/
+sudo cp -f /vagrant/apache_virtual_host/000-default.conf /etc/apache2/sites-enabled/
+
+# SSL Cert
+sudo mkdir /etc/apache2/ssl
+
+# Set our CSR variables
+SUBJ="
+C=IT
+ST=Italy
+O=FANCHO
+localityName=Bologna
+commonName=127.0.0.1
+organizationalUnitName=DevOPS
+CN=127.0.0.1
+"
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt -subj "$(echo -n "$SUBJ" | tr "\n" "/")"
+
+sudo cp -f /vagrant/apache_virtual_host/default-ssl.conf /etc/apache2/sites-enabled/
+
+#sudo a2ensite default-ssl.conf
+sudo a2enmod ssl
+
+sudo service apache2 restart
